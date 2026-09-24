d376984575d4df2230257d4dbbe69525d993fbc5
LAPTOP-BSTHBLSA\academia <wonderwander2010@gmail.com>
2026-09-24 16:28:08 +0900

    chapter4_practice

---
+++ chapter_04/practice/ques_06/paractice_1.java
@@ -0,0 +1,9 @@
+package chapter_04.practice.ques_06;
+
+public class paractice_1 {
+    public static void main(String[] var0) {
+        System.out.println("はじめまして");
+        System.out.println("私の名前は");
+        System.out.println("げんきです");
+    }
+}
