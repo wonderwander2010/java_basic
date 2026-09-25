882012a9eb44747f9f2f0f46677c0cbfda7fed64
LAPTOP-BSTHBLSA\academia <wonderwander2010@gmail.com>
2026-09-25 11:09:17 +0900

    spell_fix

--- chapter_04/practice/ques_06/practice_1.java
+++ chapter_04/practice/ques_06/practice_1.java
@@ -1,6 +1,6 @@
 package chapter_04.practice.ques_06;
 
-public class paractice_1 {
+public class practice_1 {
     public static void main(String[] var0) {
         System.out.println("はじめまして");
         System.out.println("私の名前は");
